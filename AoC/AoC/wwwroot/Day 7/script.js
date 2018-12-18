9191e908219601b043f580416c49efe6a58cd934
Antoine Granpre Moliere <a.granpre@gmail.com>
2018-12-18 22:19:41 +0100

    Started Day7 second star

--- AoC/AoC/wwwroot/Day 7/script.js
+++ AoC/AoC/wwwroot/Day 7/script.js
@@ -22,7 +22,7 @@ window.module = function () {
         var lineContents = line.trim();
         if (lineContents.length > 0) {
             var lineParts = lineContents.split(' must be finished before step ');
-            var stepMustBeFinishedPart = lineParts[0]; 
+            var stepMustBeFinishedPart = lineParts[0];
             var stepCanBeginPart = lineParts[1];
             result.previousStep = stepMustBeFinishedPart.substr(stepMustBeFinishedPart.length - 1, 1);
             result.step = stepCanBeginPart.substr(0, 1);
@@ -49,16 +49,17 @@ window.module = function () {
 
         return result;
     };
-    
-    var getFirstCompletableStep = function (instructions, finishedSteps) {
+
+    var getFirstCompletableStep = function (instructions, finishedSteps, busySteps) {
         var result = '';
         var completableSteps = [];
         for (var i = 0; i < instructions.length; i++) {
             var instruction = instructions[i];
-            if (finishedSteps.indexOf(instruction.step) === -1 
-                && allPreviousStepsCompleted(instruction.step, instructions, finishedSteps))
-            {
-                completableSteps.push(instruction.step);
+            if (finishedSteps.indexOf(instruction.step) === -1
+                && allPreviousStepsCompleted(instruction.step, instructions, finishedSteps)) {
+                if (typeof busySteps === 'undefined' || busySteps.indexOf(instruction.step) === -1) {
+                    completableSteps.push(instruction.step);
+                }
             }
         }
         completableSteps.sort();
@@ -68,7 +69,7 @@ window.module = function () {
         return result;
     };
 
-    var getFirstStep = function (instructions, finishedSteps) {
+    var getFirstStep = function (instructions, finishedSteps, busySteps) {
         var result = '';
         var previousOnlySteps = [];
         for (var i = 0; i < instructions.length; i++) {
@@ -82,7 +83,10 @@ window.module = function () {
                     break;
                 }
             }
-            if (previousStepFound === false && finishedSteps.indexOf(instruction.previousStep) === -1) {
+            if (previousStepFound === false
+                && finishedSteps.indexOf(instruction.previousStep) === -1
+                && (typeof busySteps === 'undefined'
+                    || busySteps.indexOf(instruction.previousStep) === -1)) {
                 previousOnlySteps.push(instruction.previousStep);
             }
         }
@@ -92,7 +96,52 @@ window.module = function () {
         }
         return result;
     };
-    
+
+    function createWorker(id) {
+        var result = {
+            id: id,
+            workingOnStep: null,
+            secondsUntilCompletion: 0
+        };
+        return result;
+    };
+
+    function createWorkers(count) {
+        var result = [];
+        for (var i = 0; i < count; i++) {
+            var worker = createWorker(i);
+            result.push(worker);
+        }
+        return result;
+    };
+
+    function getStepValue(nextStep) {
+        var result = nextStep.charCodeAt(0) - 64;
+        return result;
+    };
+
+    function getNextStep(instructions, finishedSteps, workers) {
+        var busySteps = '';
+        workers.forEach(function (worker) {
+            if (worker.workingOnStep !== null) {
+                busySteps += worker.workingOnStep;
+            }
+        });
+        var nextStepA = getFirstStep(instructions, finishedSteps, busySteps);
+        var nextStepB = getFirstCompletableStep(instructions, finishedSteps, busySteps);
+        if (nextStepA.length + nextStepB.length === 0) {
+            finishedSteps = finishedSteps;
+        }
+
+        var result;
+        if (nextStepA !== "" && (nextStepB === "" || nextStepA.charCodeAt(0) < nextStepB.charCodeAt(0))) {
+            result = nextStepA;
+        } else {
+            result = nextStepB;
+        }
+        return result;
+    }
+
     var processLinesA = function (lines) {
         var result = '';
         var instructions = [];
@@ -120,23 +169,72 @@ window.module = function () {
             } else {
                 finishedSteps += nextStepB;
             }
-            
+
         }
         result = finishedSteps;
         return result;
     };
-    
+
     var processLinesB = function (lines) {
         var result = '';
-        
+        var instructions = [];
+        var steps = [];
         for (var i = 0; i < lines.length; i++) {
             var line = lines[i];
-            
+            var instruction = processLine(line);
+            instructions.push(instruction);
+            if (steps.indexOf(instruction.previousStep) === -1) {
+                steps.push(instruction.previousStep);
+            }
+            if (steps.indexOf(instruction.step) === -1) {
+                steps.push(instruction.step);
+            }
         }
-       
+        var finishedSteps = '';
+        var workers = createWorkers(5);
+        var currentTime = 0;
+        while (finishedSteps.length < steps.length) {
+            workers.forEach(function (worker) {
+                if (worker.workingOnStep !== null) {
+                    worker.secondsUntilCompletion--;
+                    if (worker.secondsUntilCompletion === 0) {
+                        finishedSteps += worker.workingOnStep;
+                        worker.workingOnStep = null;
+                        var nextStep = getNextStep(instructions, finishedSteps, workers);
+                        if (nextStep !== '') {
+                            worker.workingOnStep = nextStep;
+                            worker.secondsUntilCompletion = 60 + getStepValue(nextStep);
+                        }
+                    }
+                }
+            });
+
+            workers.forEach(function (worker) {
+                if (worker.workingOnStep === null) {
+                    var step = getNextStep(instructions, finishedSteps, workers);
+                    if (step !== '') {
+                        worker.workingOnStep = step;
+                        worker.secondsUntilCompletion = 60 + getStepValue(step);
+                    }
+                }
+            });
+
+            var infoLine = '';
+            workers.forEach(function (worker) {
+                if (worker.workingOnStep === null) {
+                    infoLine += ' .';
+                } else {
+                    infoLine += ' ' + worker.workingOnStep;
+                }
+            });
+            console.info('' + currentTime + ' ' + infoLine);
+            currentTime++;
+        }
+        currentTime--;
+        result = currentTime;
         return result;
     };
-    
+
     var pocessInputA = function (input) {
         var lines = input.split('\n');
         return processLinesA(lines);
