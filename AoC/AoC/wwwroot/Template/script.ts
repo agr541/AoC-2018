0fe35a7f2c5abdd9fa24a6c9d924259b174236ed
Antoine Granpre Moliere <a.granpre@gmail.com>
2020-12-05 18:00:02 +0100

    2020 - day 4

--- AoC/AoC/wwwroot/Template/script.ts
+++ AoC/AoC/wwwroot/Template/script.ts
@@ -1,14 +1,16 @@
 // noprotect
-window.module = function () {
+(window as any).module = function () {
     var answerA = 0;
     var answerB = 0;
     var processLine = function (line) {
         var result = 0;
         var lineContents = line.trim();
         if (lineContents.length > 0) {
+           
         }
         return result;
     };
+    
     var processLinesA = function (lines) {
         var result = 0;
         lines.forEach(function (line) {
@@ -16,6 +18,7 @@ window.module = function () {
         });
         return result;
     };
+    
     var processLinesB = function (lines) {
         var result = 0;
         lines.forEach(function (line) {
@@ -23,6 +26,7 @@ window.module = function () {
         });
         return result;
     };
+
     var getInputFromUrl = function (url, callBack) {
         var req = new XMLHttpRequest();
         req.addEventListener('readystatechange', function () {
@@ -34,33 +38,41 @@ window.module = function () {
         req.open('GET', url);
         req.send();
     };
+
     var setOutput = function (outputSelector, outputValue) {
         var output = document.querySelector(outputSelector);
         output.value = outputValue;
     };
+
     var pocessInputA = function (input) {
         var lines = input.split('\n');
         return processLinesA(lines);
     };
+
     var pocessInputB = function (input) {
         var lines = input.split('\n');
         return processLinesB(lines);
     };
+
     var initializeA = function (options) {
         var input = getInputFromUrl(options.inputUrl, function (input) {
             var output = pocessInputA(input);
             setOutput(options.outputSelector, answerA);
         });
     };
+
     var initializeB = function (options) {
         var input = getInputFromUrl(options.inputUrl, function (input) {
             var output = pocessInputB(input);
             setOutput(options.outputSelector, answerB);
         });
     };
+
     return {
         runA: initializeA,
         runB: initializeB
     };
 };
-//# sourceMappingURL=script.js.map
+
+
+
